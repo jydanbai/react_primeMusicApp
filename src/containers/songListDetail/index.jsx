02adc3baabc64c3f91f9ead92c9e0085d939ef8b
daiqiuyue <986799852@qq.com>
2019-11-26 16:34:40 +0800

    songlistdetail

--- src/containers/songListDetail/index.jsx
+++ src/containers/songListDetail/index.jsx
@@ -10,11 +10,17 @@ export default class SongListDetail extends Component {
           <div className="songlist-content">
             <div className="songlist-header">
             <div className="img-wrapper">
+              <div className="play-detail">
               <i className="iconfont iconerji"></i>
               <span className="play-num">42万</span>
+              </div>
               <img className="column-img" src="http://p1.music.126.net/hD9AlEA36BUzFMSOqf4lUQ==/109951164236954260.jpg?param=200y200" alt=""/>
             </div>
-
+            <div className="right-detail">
+            <h1>【迷幻/慢摇】Psychedelic melody</h1>
+              <img src="" alt=""/>
+              <p>一只喵喵喵星人</p>
+            </div>
             </div>
           </div>
 
