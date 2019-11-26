06638d0ebd12b1348ed6288379980b69c75cda19
daiqiuyue <986799852@qq.com>
2019-11-26 10:09:23 +0800

    songlist组件

--- src/containers/songList/index.jsx
+++ src/containers/songList/index.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-
+import   './index.css'
 class SongList extends Component {
   render() {
     return (
-      <div>
-        songList
+      <div className="column-wrapper">
+        <div className="column-item">
+         <div className="img-wrapper">
+           <i className="iconfont iconerji"></i>
+         <span className="play-num">33035</span>
+          <img className="column-img" src="http://p1.music.126.net/hD9AlEA36BUzFMSOqf4lUQ==/109951164236954260.jpg?param=200y200" alt=""/>
+         </div>
+          <p className="column-title">【迷幻/慢摇】Psychedelic melody</p>
+        </div>
+        <div className="column-item">
+         <div className="img-wrapper">
+         <i className="iconfont iconerji"></i>
+         <span className="play-num">33035</span>
+          <img className="column-img" src="http://p1.music.126.net/hD9AlEA36BUzFMSOqf4lUQ==/109951164236954260.jpg?param=200y200" alt=""/>
+         </div>
+          <p className="column-title">【迷幻/慢摇】Psychedelic melody</p>
+        </div>
+        <div className="column-item">
+         <div className="img-wrapper">
+         <i className="iconfont iconerji"></i>
+         <span className="play-num">33035</span>
+          <img className="column-img" src="http://p1.music.126.net/hD9AlEA36BUzFMSOqf4lUQ==/109951164236954260.jpg?param=200y200" alt=""/>
+         </div>
+          <p className="column-title">【迷幻/慢摇】Psychedelic melody</p>
+        </div>
       </div>
     );
   }
