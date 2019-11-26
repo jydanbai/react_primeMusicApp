bcb6bc2f5116ce5f78dd62ed0584c95ebcafd2af
JinYong <1520650026@qq.com>
2019-11-26 10:21:41 +0800

    sddsdad

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {Router,Switch,Route,Redirect} from 'react-router-dom';
 import './App.css';
 import routes from './config/routes';
 import history from './history';
-import Header from './components/header';
+// import Header from './components/header';
+// import Header from './containers/rankList';
 import 'antd-mobile/dist/antd-mobile.css';
 
 export default class App extends Component {
@@ -14,13 +15,13 @@ export default class App extends Component {
       <Router history={history}>
         <Switch> {/* /login/xxx   默认使用不完全匹配 | 使用第一个匹配的路由 */}
           <Redirect from="/" to="/home" exact/>
-            <Header>
+            {/* <Header> */}
               <Switch>
                 {
                   routes.map(route => <Route {...route} key={route.path}/>)
                 }
               </Switch>
-            </Header>
+            {/* </Header> */}
         </Switch>
       </Router>
     )
--- src/containers/rankList/index.jsx
+++ src/containers/rankList/index.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import './index.less'
 
 class RankList extends Component {
   render() {
     return (
       <div>
-        rankList
+        <header className="header">
+          <div className="header-left"></div>
+          <div className="header-title">排行榜</div>
+          <div className="header-right"></div>
+        </header>
+        <div className="rank-wrapper">
+          <div className="rank-content">
+            <h1 className="title-one">官方榜单</h1>
+            <ul className="row-list">
+              <li className="row-item">
+                <div className="item-hd">
+                  <img src="https://p1.music.126.net/DrRIg6CrgDfVLEph9SNh7w==/18696095720518497.jpg?param=150y150" alt=""/>
+                  <p>每天更新</p>
+                </div>
+                <div className="row-item-bd">
+                  <p>喜欢你</p>
+                  <p>你把我灌醉</p>
+                  <p>明天会更好</p>
+                </div>
+              </li>
+              <li className="row-item"></li>
+              <li className="row-item"></li>
+              <li className="row-item"></li>
+              <li className="row-item"></li>
+              <li className="row-item"></li>
+            </ul>
+
+          </div>
+        </div>
       </div>
     );
   }
