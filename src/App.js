274d2d82b03a177f1abe9103ab5dcb9897370a97
daiqiuyue <986799852@qq.com>
2019-11-26 18:49:29 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import {Router,Switch,Route,Redirect} from 'react-router-dom';
 
 import './App.css';
 // import routes from './config/routes';
-import history from './history';
+import hashHistory from './history';
 import Header from './components/header';
 import Home from './containers/home';
 import rankList from './containers/rankList';
 import search from './containers/search';
 import SongList from './containers/songList';
-import SongListDetail from './containers/songListDetail'
 import Skin from './containers/skin';
 // import 'antd-mobile/dist/antd-mobile.css';
 import 'antd-mobile/dist/antd-mobile.css';
@@ -18,14 +17,13 @@ export default class App extends Component {
 
   render () {
     return (
-      <Router history={history}>
+      <Router history={hashHistory}>
             <Header/>
             <Switch>
               <Route path='/home' component={Home}></Route>
               <Route path='/ranklist' component={rankList}></Route>
               <Route path='/search' component={search}></Route>
               <Route path='/songlist' component={SongList}></Route>
-              <Route path='/songlistdetail' component={SongListDetail}></Route>
               <Route path='/skin' component={Skin}></Route>
               <Redirect from="/" to="/home" exact/>
             </Switch>
