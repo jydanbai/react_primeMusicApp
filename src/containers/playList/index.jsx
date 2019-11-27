bb29de5b769b7ad3d0c44c911616a6f26f91885d
JinYong <1520650026@qq.com>
2019-11-27 17:49:24 +0800

    a

--- src/containers/playList/index.jsx
+++ src/containers/playList/index.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import BScroll from 'better-scroll'
+import {getPlaylistDetail} from '../../api'
+import { HTTP_OK } from "../../config"
+import {formatPlayCount} from '../../common/util';
 import './index.less'
 
  class PlayList extends Component {
+   constructor(props) {
+     super(props)
+     this.state = {
+       data: {} //请求回来的歌单数据
+     }
+   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    await getPlaylistDetail(this.props.match.params.id).then(res => {
+      if (res.data.code === HTTP_OK) {
+        let data = res.data.playlist
+        // console.log(data)
+        this.setState({
+          data
+        }) 
+      }
+    })
     new BScroll('.rankDetail-wrapper', {
       scrollX: false,
       scrollY: true,
@@ -13,6 +31,9 @@ import './index.less'
   }
 
   render() {
+    const {data} = this.state
+    const {creator='',tracks=[]} = data
+    console.log(tracks)
     return (
       <div>
         <header className="rankDetail-header">
@@ -20,148 +41,45 @@ import './index.less'
             this.props.history.go(-1)
             }}
            className="header-left iconfont iconchangyongtubiao-xianxingdaochu-zhuanqu-"></div>
-          <div className="header-title">云音乐飙升榜</div>
+          <div className="header-title">{data.name}</div>
           <div className="header-right"></div>
         </header>
         <div className="rankDetail-wrapper">
           <div className="rankDetail-content">
             <div className="playList-header">
+              <div style={{backgroundImage: `url(${data.coverImgUrl})`}} className="beijingtu"></div>
               <div className="playList-header-wrapper">
-                <div className="playList-header-hd" data-play="25.8亿">
-                  <img src="http://p1.music.126.net/DrRIg6CrgDfVLEph9SNh7w==/18696095720518497.jpg?param=100y100" alt=""/>
+                <div className="playList-header-hd" data-play={formatPlayCount(data.playCount)}>
+                  <img src={data.coverImgUrl} alt=""/>
                 </div>
                 <div className="playList-header-bd">
-                  <h1>云音乐飙升榜</h1>
+                  <h1>{data.name}</h1>
                   <div className="playList-header-user">
-                    <img src="http://p1.music.126.net/QWMV-Ru_6149AKe0mCBXKg==/1420569024374784.jpg?param=50y50" alt=""/>
-                    <span>网易云音乐</span>
+                    <img src={`${creator.avatarUrl}?param=50y50`} alt=""/>
+                    <span>{creator.nickname}</span>
                   </div>
                 </div>
               </div>
             </div>
             <ul className="song-wrapper">
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-              <li className="song-item">
-                <div className="song-num">1</div>
-                <div className="song-info">
-                  <h2>吻别</h2>
-                  <p>
-                    <span>张学友</span>
-                    &nbsp;-&nbsp;
-                    吻别
-                  </p>
-                </div>
-              </li>
-            </ul>
+              {
+                tracks.length > 0 && tracks.map((item, index) => {
+                  return (
+                    <li key={item.id} className="song-item">
+                      <div className="song-num">{index+1}</div>
+                      <div className="song-info">
+                        <h2>{item.name}</h2>
+                        <p>
+                          <span>{item.ar[0].name}</span>
+                          &nbsp;-&nbsp;
+                          {item.al.name}
+                        </p>
+                      </div>
+                    </li>
+                  )   
+                })
+              }
+              </ul>
           </div>
         </div>
       </div>
