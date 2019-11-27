b11dee9243b955066c5a349949e58a9b3495ecbe
LaoWang-HL <1147139789@qq.com>
2019-11-27 16:02:45 +0800

    路由传参

--- src/history.js
+++ src/history.js
@@ -1,6 +1,6 @@
 /* 
 向外暴露一个history对象
 */
-import { /* createBrowserHistory */ createHashHistory } from 'history'
+import {  createBrowserHistory  /*createHashHistory*/ } from 'history'
 // 向外暴露不带#的history
-export default createHashHistory()
+export default createBrowserHistory()
